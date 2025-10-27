031d878934e3666e9046d801f83d2e660335c913
Mayank <146926037+mynk-tmr@users.noreply.github.com>
2025-10-27 06:32:42 +0530

    fix: remove unused env vars from production

--- server/utils/dotenv.ts
+++ server/utils/dotenv.ts
@@ -3,8 +3,6 @@ import { type } from 'arktype'
 const scheme = type({
   TMDB_TOKEN: 'string > 1',
   VERCEL: type('string.integer.parse').pipe(Boolean),
-  MONGO_URI: 'string.url',
-  DB_NAME: 'string',
 })
 
 export const ENV = {
--- server/utils/mongo.ts
+++
@@ -1,5 +0,0 @@
-import { MongoClient } from 'mongodb'
-import { ENV } from './dotenv'
-
-export const client = new MongoClient(ENV.MONGO_URI)
-export const db = client.db(ENV.DB_NAME)
